0e48c953d5e9591a756a8b40db7df258f91d476f
liuhao <1643977841@qq.com>
2022-04-13 21:43:38 +0800

    jdk9新特性

---
+++ JDK9/src/main/java/com/ah/test/MyInterface.java
@@ -0,0 +1,35 @@
+package com.ah.interface_test;
+
+/**
+ * <p>
+ *  在jdk9中接口可以定义私有方法，但是在jdk8中不能定义私有方法
+ *  在jdk9中接口可以定义默认方法，但是在jdk8中不能定义默认方法
+ * </p>
+ *
+ * @author Liu hao
+ * @version 1.0
+ * @date 2022/4/13 21:24
+ */
+public interface MyInterface {
+    /**
+     * 默认方法
+     */
+    default void defaultMethod() {
+        System.out.println("defaultMethod");
+    }
+
+    /**
+     * 私有方法
+     */
+    private void privateMethod() {
+        System.out.println("privateMethod");
+    }
+
+    /**
+     * 静态方法
+     * 接口中的静态方法，只能用这个接口去调用，不能用实现类去调用
+     */
+    static void staticMethod() {
+        System.out.println("staticMethod");
+    }
+}
---
+++ JDK9/src/main/java/com/ah/test/MyInterfaceTest.java
@@ -0,0 +1,40 @@
+package com.ah.interface_test;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Liu hao
+ * @version 1.0
+ * @date 2022/4/13 21:28
+ */
+public class MyInterfaceTest implements MyInterface {
+
+    public static void main(String[] args) {
+        MyInterface.staticMethod();
+        // 钻石操作符的升级 jdk8中Comparator后面的泛型参数必须填写
+        Comparator<Object> comparator = new Comparator<>() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return 0;
+            }
+        };
+        // try 资源会自动的关闭，在jdk8中一般是在finally中关闭
+        // 要求自动关闭的资源必须在try的小括号中
+        try(InputStreamReader reader = new InputStreamReader(System.in)){
+            char[] chars = new char[20];
+            int len;
+            if((len = reader.read(chars)) != -1){
+                String s = new String(chars, 0, len);
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
